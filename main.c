493c0591f6aec9e2ce3101847bc53a44448a778b
王 子都 <s1242004@u-aizu.ac.jp>
2018-07-08 19:22:12 +0900

    Initial revision

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+int main()
+{
+
+    srand((int)time(0));
+   int rand_num1 = rand()%6 + 1;
+   int rand_num2 = rand()%6 + 1;
+   char name[60];
+ 
+ printf("Rolling the dice...\n");
+ printf("Die 1:%d\n", rand_num1);
+ printf("Die 2:%d\n", rand_num2);
+ printf("Total value: %d\n",rand_num1+rand_num2);
+
+ return 0;
+}
